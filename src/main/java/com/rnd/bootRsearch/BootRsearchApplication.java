fbb1737467b5445e38f619c97311b3f3b763f685
ashimgit <ashimnb87@gmail.com>
2019-03-16 23:01:47 +0530

    my first commit

---
+++ src/main/java/com/rnd/bootRsearch/BootRsearchApplication.java
@@ -0,0 +1,13 @@
+package com.rnd.bootRsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRsearchApplication.class, args);
+	}
+
+}
